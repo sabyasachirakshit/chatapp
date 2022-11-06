85f137952531db986e6757e4e9405444618b7ae7
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-11-06 19:54:52 +0530

    templates rendered

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,11 +30,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CHANNEL_LAYERS = {
-	"default": {
-		"BACKEND": "channels.layers.InMemoryChannelLayer"
-	}
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
-
 
 
 # Application definition
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'chatapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +131,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL='chat-page'
+LOGIN_REDIRECT_URL = 'chat-page'
 
-LOGOUT_REDIRECT_URL='login-user'
+LOGOUT_REDIRECT_URL = 'login-user'
